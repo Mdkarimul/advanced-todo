3c22d8502e225a969ef0abbef8d37069de0fdefc
Mdkarimul <7mdkarimul@gmail.com>
2024-12-14 19:52:54 +0530

    commit before modal design

--- src/app/pages/dashboard/todo/todo.component.ts
+++ src/app/pages/dashboard/todo/todo.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, viewChild, ViewContainerRef } from '@angular/core';
+import { ModalComponent } from '../../modal/modal.component';
 
 @Component({
   selector: 'app-todo',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent {
+
+  vcr = viewChild('container',{read:ViewContainerRef});
+  showModal(){
+    this.vcr()?.clear();
+    this.vcr()?.createComponent(ModalComponent);
+  }
 
 }
